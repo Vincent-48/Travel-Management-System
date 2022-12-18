4dfc9d4a12e4b52e0e8b708394998ca4026b0293
Vincent-48 <vincentochieng33@gmail.com>
2022-12-18 11:22:24 +0300

    initial commit

--- issuetickets.php
+++ issuetickets.php
@@ -93,7 +93,15 @@ if($query->rowCount() > 0)
 {
 foreach($results as $result)
 {	?>
-
+<tr align="center">
+<td ><?php echo htmlentities($cnt);?></td>
+<td width="100">#TKT-<?php echo htmlentities($result->id);?></td>
+<td><?php echo htmlentities($result->Issue);?></td>
+<td width="300"><?php echo htmlentities($result->Description);?></td>
+<td><?php echo htmlentities($result->AdminRemark);?></td>
+<td width="100"><?php echo htmlentities($result->PostingDate);?></td>
+<td width="100"><?php echo htmlentities($result->AdminremarkDate);?></td>
+</tr>
 <?php $cnt=$cnt+1; }} ?>
 	</table>
 		
